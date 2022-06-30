a167b155c482c2226252381de222ba49b0bd3558
Courtney Perigo <11284316+agentdanger@users.noreply.github.com>
2022-06-30 01:31:50 -0500

    Update main.py

--- api-mlresponse/main.py
+++ api-mlresponse/main.py
@@ -1,17 +1,13 @@
-from flask import Flask
-from flask_restful import Api, Resource
+from flask import Flask, jsonify, request
   
-app =   Flask(__name__)
+app = Flask(__name__)
   
-api =   Api(app)
   
-class HelloWorld(Resource):
-	def get(self):
-		data={"data":"Hello World"}
-		return data
-  
-api.add_resource(HelloWorld,'/hello')
-  
+@app.route('/hello', methods=['GET'])
+def helloworld():
+    if(request.method == 'GET'):
+        data = {"data": "Hello World"}
+        return jsonify(data)  
   
 if __name__=='__main__':
-	app.run(port=8080)
+	app.run(host="localhost", port=8080, debug=True)
