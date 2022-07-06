a9350602d84bb588de4c44141ef914a079d51007
Courtney Perigo <11284316+agentdanger@users.noreply.github.com>
2022-07-05 21:15:51 -0500

    multiple saves

--- myrepolib/repomod.py
+++ myrepolib/repomod.py
@@ -1,2 +1,2 @@
 def myfunc():
-    return 1
+    return 1 
--- tests/test_myrepo.py
+++ tests/test_myrepo.py
@@ -2,4 +2,4 @@ from myrepolib import repomod
 
 def test_func():
     result = repomod.myfunc()
-    assert result == 1
+    assert result == 1 
