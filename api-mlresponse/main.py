70a2d2f61757b546464c59b782718cda0a33a74f
Courtney Perigo <11284316+agentdanger@users.noreply.github.com>
2022-07-12 14:23:07 -0500

    Update main.py

--- api-mlresponse/main.py
+++ api-mlresponse/main.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request, render_template
-from google.cloud import bigquery
+from google.cloud import bigquery, storage
   
 app = Flask(__name__)
 
@@ -10,6 +10,9 @@ client = bigquery.Client()
 table_id_dev = "ad-forecasting-nu.d_ad_forecasting_nu.t_ad_forecasting_data_dev"
 
 table_id_prod = "ad-forecasting-nu.d_ad_forecasting_nu.t_ad_forecasting_data_prod"
+
+# Configure this environment variable via app.yaml
+CLOUD_STORAGE_BUCKET = "ad-forecasting-nu.appspot.com"
 
 sql_dev = """
 CREATE TABLE IF NOT EXISTS `{0}` 
@@ -70,6 +73,40 @@ job_prod.result()  # Waits for the query to finish.
 def index():
      # Set The upload HTML template '\templates\index.html'
     return render_template('index.html')
+
+@app.route('/file_uploaded', methods=['POST'])
+def file_uploaded() -> str:
+    """Process the uploaded file and upload it to Google Cloud Storage."""
+    uploaded_file = request.files.get('file')
+
+    if not uploaded_file:
+        return 'No file uploaded.', 400
+
+    # Create a Cloud Storage client.
+    gcs = storage.Client()
+
+    # Get the bucket that the file will be uploaded to.
+    bucket = gcs.get_bucket(CLOUD_STORAGE_BUCKET)
+
+    # Create a new blob and upload the file's content.
+    blob = bucket.blob(uploaded_file.filename)
+
+    blob.upload_from_string(
+        uploaded_file.read(),
+        content_type=uploaded_file.content_type
+    )
+
+    
+    return blob.path
+
+
+@app.errorhandler(500)
+def server_error(e: Union[Exception, int]) -> str:
+    logging.exception('An error occurred during a request.')
+    return """
+    An internal error occurred: <pre>{}</pre>
+    See logs for full stacktrace.
+    """.format(e), 500
   
 @app.route('/', methods=['GET'])
 def helloworld():
